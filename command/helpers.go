fa542b916fa4333295b9447f48e3a201abde88f4
alexbarksdale <ale.barksdale@gmail.com>
2020-07-16 21:15:44 -0500

    [Add] Helper documentation

--- command/helpers.go
+++ command/helpers.go
@@ -11,6 +11,8 @@ import (
 	"github.com/derekparker/trie"
 )
 
+// LoadBubbles reads the BublSavePath and unmarshals the bubble.json file.
+// It also generates a trie tree of all the aliases for improved search speed.
 func LoadBubbles() ([]Bubble, *trie.Trie) {
 	file, err := ioutil.ReadFile(util.BublSavePath)
 	if err != nil {
@@ -28,6 +30,7 @@ func LoadBubbles() ([]Bubble, *trie.Trie) {
 	return bubbles, t
 }
 
+// BubbleExist validates a bubble by taking in a trie tree and alias.
 func BubbleExist(t *trie.Trie, alias string) bool {
 	node, found := t.Find(strings.ToLower(alias))
 	if found {
@@ -41,6 +44,7 @@ func BubbleExist(t *trie.Trie, alias string) bool {
 	return false
 }
 
+// FindBubbleSrc locates a file/directory of a bubble by taking in a trie tree and alias.
 func FindBubbleSrc(t *trie.Trie, alias string) (string, bool) {
 	var src string
 
@@ -55,6 +59,8 @@ func FindBubbleSrc(t *trie.Trie, alias string) (string, bool) {
 	return src, false
 }
 
+// RemoveBubble iterates over a []Bubble to find a matching bubble alias
+// and removes it by swapping it with the last item and returning n-1 items.
 func RemoveBubble(b []Bubble, alias string) ([]Bubble, bool) {
 	for i, v := range b {
 		if strings.ToLower(v.Alias) == strings.ToLower(alias) {
